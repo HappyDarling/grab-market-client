05bf3eca808ea10e6cb72ef0c37d98f704cd2d47
HappyDarling <linsyay@gmail.com>
2021-08-27 18:07:16 +0900

    project init

---
+++ src/main/index.js
@@ -0,0 +1,54 @@
+import "./index.css";
+import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://d65714a2-c9c1-4fd0-b86b-d64bc952f006.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
+      </div>
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/product/${product.id}`}>
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img class="product-avatar" src="images/icons/avatar.png" />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/product/index.js
@@ -0,0 +1,47 @@
+import "./index.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function ProductPage() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(function () {
+    axios
+      .get(
+        "https://d65714a2-c9c1-4fd0-b86b-d64bc952f006.mock.pstmn.io/products/" +
+          id
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product == null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
